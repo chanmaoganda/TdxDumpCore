27a7c473a1e16df9527106cd66660946fb6ca28b
chanmaoganda <avaniawang@outlook.com>
2024-09-30 22:57:17 +0800

    better builder for DayLine

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Core library for dumping TDX .day files, with optional feature to calculate dif,
 ## Usage
 
 ```rust
-let builder = DayLineBuilder::from_path("../shlday/sh000001.day")?.query_days(10)?;
-let day_line = builder.build()?;
+let builder = DayLineBuilder::from_path("../shlday/sh000001.day")?
+    .query_days(10);
+let day_line = builder.build();
 println!("{:?}", day_line);
 ```
--- src/model/builder/day_line_builder.rs
+++ src/model/builder/day_line_builder.rs
@@ -20,31 +20,36 @@ impl DayLineBuilder {
         Ok(Self { file, max_days })
     }
 
-    pub fn query_days(mut self, days: u64) -> anyhow::Result<Self> {
-        let pos_offset = (days * DAY_SIZE as u64) as i64;
-        self.file.seek( SeekFrom::End(0 - pos_offset) )?;
+    pub fn query_days(mut self, days: u64) -> Self {
         if self.max_days > days {
+            // take the smaller one between the two values
             self.max_days = days;
         }
-        Ok(self)
+
+        let pos_offset = (self.max_days * DAY_SIZE as u64) as i64;
+        self.file.seek( SeekFrom::End(0 - pos_offset) ).unwrap();
+        
+        self
     }
 
-    pub fn build(mut self) -> anyhow::Result<DayLine> {
+    pub fn build(mut self) -> DayLine {
         let mut buffer = [0u8; DAY_SIZE];
         let mut day_line = Vec::with_capacity(self.max_days as usize);
 
         for _ in 0..self.max_days {
-            self.file.read_exact(buffer.as_mut())?;
+            self.file.read_exact(buffer.as_mut()).unwrap();
             let date = byteorder::LE::read_u32(&buffer[0..4]);
             let open = byteorder::LE::read_u32(&buffer[4..8]) as f32 / 100f32;
             let high = byteorder::LE::read_u32(&buffer[8..12]) as f32 / 100f32;
             let low = byteorder::LE::read_u32(&buffer[12..16]) as f32 / 100f32;
             let close = byteorder::LE::read_u32(&buffer[16..20]) as f32 / 100f32;
+            // last 12 bytes consists of 4 bytes of turnover, 4 bytes of volume, and last 4 bytes reserved
+            // those bytes are not needed now, so we drop these bytes
             let daily_data = DailyData::new(date, open, high, low, close);
             day_line.push(daily_data);
         }
 
-        Ok(DayLine::new(day_line))
+        DayLine::new(day_line)
     }
 
 }
@@ -52,8 +57,9 @@ impl DayLineBuilder {
 
 #[test]
 fn builder_test() -> anyhow::Result<()> {
-    let builder = DayLineBuilder::from_path("../shlday/sh000001.day")?.query_days(10)?;
-    let day_line = builder.build()?;
+    let builder = DayLineBuilder::from_path("../shlday/sh000001.day")?
+        .query_days(10);
+    let day_line = builder.build();
     dbg!(day_line);
     Ok(())
 }
